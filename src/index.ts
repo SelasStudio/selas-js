0d0f3669f751dc0842c79578b47f34b1874e583a
romeo-selas <102542175+romeo-selas@users.noreply.github.com>
2022-10-18 17:12:23 +0200

    signIn now returns the result

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,7 @@ export class SelasClient {
   }
 
   async signIn(email: string, password: string) {
-    await this.supabase.auth.signInWithPassword({ email, password });
+    return await this.supabase.auth.signInWithPassword({ email, password });
   }
 
   async getCustomer(external_id: string) {
